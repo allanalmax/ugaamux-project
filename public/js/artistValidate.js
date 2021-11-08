7ac1ac930ff71c88dbc5adc316542af85a18593b
allan almax <allanalmax13@gmail.com>
2021-11-08 11:12:00 +0300

    Changes made on the validation process on the forms

--- public/js/artistValidate.js
+++ public/js/artistValidate.js
@@ -1,83 +1,101 @@
 const formValidate = {
     data() {
         return {
-            firstNameError: false,
-            // errors: [],
-            // artists: [],
-            // artist: {
-            //     fullName: "",
-            //     stageName: "",
-            //     dateOfbirth: "",
-            //     baseLocation: "",
-            //     profile: "",
-            //     NIN: "",
-            //     currentLocation: "",
-            //     gender: "",
-            //     numberOfalbums: "",
-            //     artistID: "",
-            //     dateOfstart: "",
-            //     contact: ""
-            // }
+            fullNameError: false,
+            stageNameError: false,
+            dateOfBirthError: false,
+            baseLocationError: false,
+            profileError: false,
+            NINError: false,
+            currentLocationError: false,
+            genderError: false,
+            numberOfAlbumsError: false,
+            artistIDError: false,
+            dateOfStartError: false,
+            contactError: false,
+            artists: [],
+            artist: {
+                fullName: "",
+                stageName: "",
+                dateOfBirth: "",
+                baseLocation: "",
+                profile: "",
+                NIN: "",
+                currentLocation: "",
+                gender: "",
+                numberOfAlbums: "",
+                artistID: "",
+                dateOfStart: "",
+                contact: ""
+            }
         };
     },
     methods: {
         registerArtist(event) {
             event.preventDefault();
-            // this.errors = [];
-
-            // if (!this.artist.fullName) {
-            //     this.errors.push("Full name required")
-            // }
-
-            // if (!this.artist.stageName) {
-            //     this.errors.push("Stage name required")
-            // }
-
-            // if (!this.errors.length) {
-            //     return true;
-            // }
-            // e.preventDefault();
-            // artist: {
-            //     fullName: {required},
-            //     stageName: {required}
-            // }
+            
             if (!this.artist.fullName) {
-                if (!this.firstNameError) {
-                    this.firstNameError = true;
+                if (!this.fullNameError) {
+                    this.fullNameError = true;
                 } 
             }
-            // if (!this.artist.stageName) {
-            //     alert("Stage name required")
-            // }
-            // if (!this.dateOfbirth) {
-            //     alert("Date of Birth required")
-            // }
-            // if (!this.baseLocation) {
-            //     alert("Base location required")
-            // }
-            // if (!this.profile) {
-            //     alert("Profile required")
-            // }
-            // if (!this.NIN) {
-            //     alert("NIN required")
-            // }
-            // if (!this.currentLocation) {
-            //     alert("Current Location required")
-            // }
-            // if (!this.numberOfalbums) {
-            //     alert("Number of albums required")
-            // }
-            // if (!this.artistID) {
-            //     alert("Artist ID required")
-            // }
-            // if (!this.dateOfstart) {
-            //     alert("Date of start required")
-            // }
-            // if (!this.contact) {
-            //     alert("Contact required")
-            // }
+            if (!this.artist.stageName) {
+                if (!this.stageNameError) {
+                    this.stageNameError = true;
+                }
+            }
+            if (!this.artist.dateOfBirth) {
+                if (!this.dateOfBirthError) {
+                    this.dateOfBirthError = true;
+                }
+            }
+            if (!this.artist.baseLocation) {
+                if (!this.baseLocationError) {
+                    this.baseLocationError = true;
+                }
+            }
+            if (!this.artist.profile) {
+                if (!this.profileError) {
+                    this.profileError = true;
+                }
+            }
+            if (!this.artist.NIN) {
+                if (!this.NINError) {
+                    this.NINError = true;
+                }
+            }
+            if (!this.artist.currentLocation) {
+                if (!this.currentLocationError) {
+                    this.currentLocationError = true;
+                }
+            }
+            if (!this.artist.gender) {
+                if (!this.genderError) {
+                    this.genderError = true;
+                }
+            }
+            if (!this.artist.numberOfAlbums) {
+                if (!this.numberOfAlbumsError) {
+                    this.numberOfAlbumsError = true;
+                }
+            }
+            if (!this.artist.artistID) {
+                if (!this.artistIDError) {
+                    this.artistIDError = true;
+                }
+            }
+            if (!this.artist.dateOfStart) {
+                if (!this.dateOfStartError) {
+                    this.dateOfStartError = true;
+                }
+            }
+            if (!this.artist.contact) {
+                if (!this.contactError) {
+                    this.contactError = true;
+                }
+            }
         },
-    };
+    }
 };
 
 Vue.createApp(formValidate).mount("#app");
